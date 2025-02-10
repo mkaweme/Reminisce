f4eee16dd72dd770c60851689099267d88705b90
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-02-10 22:59:02 +0200

    first commit

---
+++ app/splits.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SIZES = [
+  "3 Piece",
+  "4 Piece Nice | 90CM * 120CM",
+  "4 Piece Joy | 70CM * 120CM",
+  "5 Piece Sweet | 90CM * 150CM",
+  "6 Piece Home | 3 * 50CM L * 25CM W | 3 * 25CM * 25CM",
+];
+const splits = () => {
+  return (
+    <View>
+      <Text>splits</Text>
+    </View>
+  );
+};
+
+export default splits;
+
+const styles = StyleSheet.create({});
