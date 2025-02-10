ef7c967883848de526c2500dd3e76ae32c78f4be
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-02-10 22:57:54 +0200

    5 page screen stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
+import React from "react";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="products" options={{ title: "Products" }} />
+      <Stack.Screen name="portraits" options={{ title: "Portraits" }} />
+      <Stack.Screen name="uploadPortrait" options={{ title: "Upload Portrait" }} />
+    </Stack>  
+  );
 }
