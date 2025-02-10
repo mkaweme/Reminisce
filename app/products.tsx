ca00c291629649aca00c36b6f031a521b48fb7d7
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-02-10 23:00:33 +0200

    refactor: removed portraits to own page

--- app/products.tsx
+++ app/products.tsx
@@ -1,20 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Link, RelativePathString } from "expo-router";
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+const PRODUCTS: { id: number, title: string, backgroundColor: string, link: RelativePathString, image: string }[] = [
+  {
+    id: 1,
+    title: "PORTRAITS",
+    backgroundColor: "#e7d4b4",
+    link: "/portraits" as RelativePathString,
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 2,
+    title: "Splits",
+    backgroundColor: "skyblue",
+    link: "/splits" as RelativePathString,
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 3,
+    title: "Combos",
+    backgroundColor: "steelblue",
+    link: "/combos" as RelativePathString,
+    image: "https://picsum.photos/200/300",
+  },
+];
 const products = () => {
   return (
-    <View>
-      <View>
-        <Text>Portraits</Text>
-        <View></View>
-        <Text>A1</Text>
-        <Text>A2</Text>
-        <Text>A3</Text>
-        <Text>40 * 40</Text>
-        <Text>50 * 50</Text>
-        <Text>30 * 30</Text>
-        <Text>A4 * 2</Text>
-      </View>
+    <FlatList 
+      data={PRODUCTS} 
+      keyExtractor={(item) => item.id.toString()} 
+      renderItem={({ item }) => 
+        <Item 
+          link={item.link}
+          title={item.title} 
+          image={item.image} 
+          style={styles.section} 
+          backgroundColor={item.backgroundColor}
+        />
+      } 
+    >         
       <View>
         <Text>Splits</Text>
         <View></View>
@@ -35,10 +60,62 @@ const products = () => {
         <Text>6 Piece</Text>
         <Text>7 Piece Warm | 1 * A1 | 6 * 25CM * 25CM</Text>
       </View>
-    </View>
+    </FlatList>
   );
 };
 
 export default products;
 
-const styles = StyleSheet.create({});
+type ItemProps = { title: string, image: string, style : object, backgroundColor: string, link: RelativePathString };
+
+const Item = ({ title, image, style, backgroundColor, link }: ItemProps) => (
+  <View style={{ ...style, backgroundColor: backgroundColor }}>
+    <Text style={styles.title}>{title}</Text>
+    <Image style={styles.portrait} source={{ uri:image }} />
+    <Link href={link} asChild>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>See Products</Text>
+      </TouchableOpacity>
+    </Link>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container : {
+    display: "flex",
+    flexDirection: "column",
+  },
+  section: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  portrait: {
+    width: 200,
+    height: 300,
+    margin: 20,
+    borderColor: "#FFFFFF",
+    borderWidth: 5,
+  },
+  title : {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    margin: 40,
+  },
+  button: {
+    margin: 30,
+    width: 150,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    alignSelf: "center",
+    backgroundColor: "#3d7794",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
