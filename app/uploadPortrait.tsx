a0afaec34289c009a9dc77351e33103a46e93734
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-02-14 05:36:02 +0200

    updated functionality and styling

--- app/uploadPortrait.tsx
+++ app/uploadPortrait.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useLocalSearchParams } from "expo-router";
 import logo_mini from "../assets/images/logo_mini.jpg";
 
-const uploadPortrait = () => {
+const UploadPortrait = () => {
 
   const { size, price, aspectRatio: aspectRatioParam } = useLocalSearchParams();
-  const aspectRatio = Array.isArray(aspectRatioParam) ? parseInt(aspectRatioParam[0]) : parseInt(aspectRatioParam);
+  const aspectRatio = Array.isArray(aspectRatioParam) ? parseFloat(aspectRatioParam[0]) : parseFloat(aspectRatioParam);
   const [image, setImage] = useState<string | null> (null);
+  const [image2, setImage2] = useState<string | null> (null);
 
+  //Create a function to selecting an image from the device
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
@@ -20,32 +22,47 @@ const uploadPortrait = () => {
 
     //If an images was selected and the process wasn't cancelled
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
+      return result.assets[0].uri;
+    } else return null;
   };
-
+  
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container} contentContainerStyle={{ alignItems: "center" }}>
       <View style={styles.button}>
         <Text style={styles.buttonText}>{size}</Text>
         <Text style={styles.buttonText}> : </Text>
         <Text style={styles.buttonText}>K{price}</Text>
       </View>
-      <View style={styles.imageContainer}>
-        <Image source={image ? { uri: image } : logo_mini} style={{ width: 300, height: 300 * aspectRatio }} />
+      <View>
+        <View>
+          <View style={styles.imageContainer}>
+            <Image source={image ? { uri: image } : logo_mini} style={{ width: 300, height: 300 * aspectRatio, resizeMode: "contain" }} />
+          </View>
+          <Button title="Select Image" onPress={async () => setImage(await pickImage())} />
+        </View>
+        {
+          size == "A4 X 2" 
+            ? (
+              <View>
+                <View style={styles.imageContainer}>
+                  <Image source={image2 ? { uri: image2 } : logo_mini} style={{ width: 300, height: 300 * aspectRatio, resizeMode: "contain" }} />
+                </View>
+                <TouchableOpacity style={styles.button2} onPress={async () => setImage2(await pickImage())}>
+                  <Text style={{ color: "#fff" }}>Select Image</Text>
+                </TouchableOpacity>
+              </View>
+            ) : null
+        } 
       </View>
-      <Button title="Select Image" onPress={pickImage} />
-    </View>
+    </ScrollView>
   );
 };
 
-export default uploadPortrait;
+export default UploadPortrait;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "space-around",
   },
   button: {
     display: "flex",
@@ -69,9 +86,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     shadowColor: "#000000",
-    shadowOffset: { width: 5, height: 5 },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    elevation: 10,
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.5,
+    elevation: 5,
+    margin: 20
   },
+  button2: {
+    display: "flex",
+    alignItems: "center",
+    color: "white",
+    marginBottom: 20,
+    backgroundColor: "#5c799a", 
+    padding: 10, 
+  }
 });
