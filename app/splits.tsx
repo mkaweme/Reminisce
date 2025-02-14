561b48593453d52e4d7986e90acc732a2d9515df
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-02-14 05:35:40 +0200

    updated sizes

--- app/splits.tsx
+++ app/splits.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const SIZES = [
-  "3 Piece",
-  "4 Piece Nice | 90CM * 120CM",
-  "4 Piece Joy | 70CM * 120CM",
-  "5 Piece Sweet | 90CM * 150CM",
-  "6 Piece Home | 3 * 50CM L * 25CM W | 3 * 25CM * 25CM",
+const SPLITS = [
+  { size: "3 Piece DINE | 70CM * 30CM", price: 650, aspectRatio: 2.33 },
+  { size: "4 Piece Nice | 90CM * 120CM", price: 780, aspectRatio: 1.33 },
+  { size: "4 Piece Joy | 70CM * 120CM", price: 750, aspectRatio: 1.71 },
+  { size: "5 Piece Sweet | 90CM * 150CM", price: 850, aspectRatio: 1.66 },
+  { size: "6 Piece Home | 3 * 50CM L * 25CM W | 3 * 25CM * 25CM", price: 780, aspectRatio: 2 },
 ];
-const splits = () => {
+const Splits = () => {
   return (
     <View>
       <Text>splits</Text>
@@ -16,6 +16,6 @@ const splits = () => {
   );
 };
 
-export default splits;
+export default Splits;
 
 const styles = StyleSheet.create({});
