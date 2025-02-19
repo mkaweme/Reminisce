1bbc464321c4059a1208b815d2facd7532f57008
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-02-19 08:29:54 +0200

    added combo and split screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="combos" options={{ title: "Combos" }} />
       <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="products" options={{ title: "Products" }} />
       <Stack.Screen name="portraits" options={{ title: "Portraits" }} />
+      <Stack.Screen name="products" options={{ title: "Products" }} />
+      <Stack.Screen name="splits" options={{ title: "Splits" }} />
       <Stack.Screen name="uploadPortrait" options={{ title: "Upload Portrait" }} />
     </Stack>  
   );
