b6090c7edabece97fb5b3d36f5815a9e2f86e66d
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-12 01:10:20 +0200

    added redux provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import store from "./store";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="combos" options={{ title: "Combos" }} />
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="order" options={{ title: "Order" }} />
-      <Stack.Screen name="portraits" options={{ title: "Portraits" }} />
-      <Stack.Screen name="products" options={{ title: "Products" }} />
-      <Stack.Screen name="splits" options={{ title: "Splits" }} />
-      <Stack.Screen name="uploadPortrait" options={{ title: "Upload Portrait" }} />
-    </Stack>  
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen name="combos" options={{ title: "Combos" }} />
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="order" options={{ title: "Order" }} />
+        <Stack.Screen name="portraits" options={{ title: "Portraits" }} />
+        <Stack.Screen name="products" options={{ title: "Products" }} />
+        <Stack.Screen name="splits" options={{ title: "Splits" }} />
+        <Stack.Screen name="uploadPortrait" options={{ title: "Upload Portrait" }} />
+      </Stack>  
+    </Provider>
   );
 }
