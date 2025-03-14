8cf00d9f9edafb7efe1b8cd3203d630d66c022d6
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-14 08:53:41 +0200

    moved combo items to components/combos

--- app/combos.tsx
+++ app/combos.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {  StyleSheet, ScrollView } from "react-native";
-import Cherry from "./components/cherry";
-import Time from "./components/time";
-import Warm from "./components/warm";
-import Moments from "./components/moments";
-import Fun from "./components/fun";
-import Growth from "./components/growth";
+import Cherry from "./components/combos/cherry";
+import Time from "./components/combos/time";
+import Warm from "./components/combos/warm";
+import Moments from "./components/combos/moments";
+import Fun from "./components/combos/fun";
+import Growth from "./components/combos/growth";
 
 const Combos: React.FC = () => {
 
