8bcd084fffbd347896b4788fa4b73b7469799287
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-14 08:58:45 +0200

    moded splits to components/splits

--- app/splits.tsx
+++ app/splits.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
-import Dine from "./components/dine";
-import Joy from "./components/joy";
-import Nice from "./components/nice";
-import Sweet from "./components/sweet";
-import Home from "./components/home";
+import Dine from "./components/splits/dine";
+import Joy from "./components/splits/joy";
+import Nice from "./components/splits/nice";
+import Sweet from "./components/splits/sweet";
+import Home from "./components/splits/home";
 
 const Splits = () => {
     
