ba32bf28e705772a7e639c46eb021b97b94b124d
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-14 08:58:25 +0200

    update products page

--- app/products.tsx
+++ app/products.tsx
@@ -1,92 +1,167 @@
 import React from "react";
-import { Link, RelativePathString } from "expo-router";
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
+import { 
+  Image, 
+  SafeAreaView, 
+  ScrollView, 
+  StyleSheet, 
+  Text, 
+  TouchableOpacity, 
+  View 
+} from "react-native";
+import { Divider } from "@rneui/base";
+import HexagonImage from "./components/hexagonImage";
 
-const PRODUCTS: { 
-  id: number, 
-  title: string, 
-  backgroundColor: string, 
-  link: RelativePathString, 
-  image: string }[] = [
-  {
-    id: 1,
-    title: "PORTRAITS",
-    backgroundColor: "#e7d4b4",
-    link: "/portraits" as RelativePathString,
-    image: "https://picsum.photos/200/300",
-  },
-  {
-    id: 2,
-    title: "Splits",
-    backgroundColor: "skyblue",
-    link: "/splits" as RelativePathString,
-    image: "https://picsum.photos/200/300",
-  },
-  {
-    id: 3,
-    title: "Combos",
-    backgroundColor: "steelblue",
-    link: "/combos" as RelativePathString,
-    image: "https://picsum.photos/200/300",
-  },
-  {
-    id: 4,
-    title: "Hexagons",
-    backgroundColor: "#cd212a",
-    link: "/hexagons" as RelativePathString,
-    image: "https://picsum.photos/200/300",
-  },
-];
 const Products = () => {
+
   return (
-    <FlatList 
-      data={PRODUCTS} 
-      keyExtractor={(item) => item.id.toString()} 
-      renderItem={({ item }) => 
-        <Item 
-          link={item.link}
-          title={item.title} 
-          image={item.image} 
-          style={styles.section} 
-          backgroundColor={item.backgroundColor}
-        />
-      } 
-    >     
-    </FlatList>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.section}>
+          <Text style={styles.canvasType}>PORTRAITS</Text>
+          <Image style={styles.portrait} source={{ uri: "https://picsum.photos/200/300" }} />
+          <Link href="/portraitSizes" asChild>
+            <TouchableOpacity style={styles.orderButton}>
+              <Text style={styles.orderButtonText}>SELECT</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+        <Divider />
+        <View style={styles.section}>
+          <Text style={styles.canvasType}>SPLITS</Text>
+          <View style={styles.splitContainer}>
+            <View style={styles.splitPreviewContainer_1}> 
+              <View style={styles.splitPreviewWindow}>
+                <Image 
+                  source={{ uri: "https://picsum.photos/200/300" }} 
+                  style={styles.splitImage_1} 
+                />
+              </View>
+            </View>
+            <View style={styles.splitPreviewContainer_2}>
+              <View style={styles.splitPreviewWindow}>
+                <Image
+                  source={{ uri: "https://picsum.photos/200/300" }} 
+                  style={styles.SplitImage_2} 
+                />
+              </View>
+            </View>
+            <View style={styles.splitPreviewContainer_1}>
+              <View style={{ width: 75, height: 140, overflow: "hidden" }}>
+                <Image 
+                  source={{ uri: "https://picsum.photos/200/300" }} 
+                  style={styles.splitImage_3} 
+                />
+              </View>
+            </View>
+            <View style={styles.splitPreviewContainer_2}>
+              <View style={styles.splitPreviewWindow}>
+                <Image 
+                  source={{ uri: "https://picsum.photos/200/300" }}
+                  style={styles.splitImage_4} 
+                />
+              </View>
+            </View>
+          </View>
+          <Link href="/splits" asChild>
+            <TouchableOpacity style={styles.orderButton}>
+              <Text style={styles.orderButtonText}>SELECT</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+        <Divider />
+        <View style={styles.section}>
+          <Text style={styles.canvasType}>COMBOS</Text>
+          <View style={styles.comboImagesContainer}>
+            <View style={styles.comboColumnImagesContainer}>
+              <View style={styles.comboImageContainer}>
+                <Image 
+                  source={{ uri : "https://picsum.photos/200/300" }} 
+                  style={styles.comboImage_1} 
+                />
+              </View>
+              <View style={styles.comboImageContainer}>
+                <Image 
+                  source={{ uri : "https://picsum.photos/200/300" }} 
+                  style={styles.comboImage_1} 
+                />
+              </View>
+            </View>
+            <View style={styles.comboColumnImagesContainer}>
+              <View style={styles.comboImageContainer}>
+                <Image 
+                  source={{ uri : "https://picsum.photos/200/300" }} 
+                  style={styles.comboImage_2} resizeMode="cover"
+                />
+              </View>
+              <View style={styles.comboImageContainer}>
+                <Image 
+                  source={{ uri : "https://picsum.photos/200/300" }} 
+                  style={styles.comboImage_2} resizeMode="cover"
+                />
+              </View>
+            </View>
+            <View style={styles.comboColumnImagesContainer}>
+              <View style={styles.comboImageContainer}>
+                <Image 
+                  source={{ uri : "https://picsum.photos/200/300" }} 
+                  style={styles.comboImage_1} 
+                />
+              </View>
+              <View style={styles.comboImageContainer}>
+                <Image 
+                  source={{ uri : "https://picsum.photos/200/300" }} 
+                  style={styles.comboImage_1} 
+                />
+              </View>
+            </View>
+          </View>
+          <Link href="/combos" asChild>
+            <TouchableOpacity style={styles.orderButton}>
+              <Text style={styles.orderButtonText}>SELECT</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+        <Divider />
+        <View style={styles.section}>
+          <Text style={styles.canvasType}>Hexagons</Text>
+          <View style={styles.hexagonImagesContainer}>
+            <View style={styles.hexagonImageContainer}>
+              <HexagonImage source={{ uri : "https://picsum.photos/200/300" }} size={150} />
+            </View>
+            <View style={styles.hexagonHorizonalContainer}>
+              <View style={styles.hexagonImageContainer}>
+                <HexagonImage source={{ uri : "https://picsum.photos/200/300" }} size={150} />
+              </View>
+              <View style={styles.hexagonImageContainer}>
+                <HexagonImage source={{ uri : "https://picsum.photos/200/300" }} size={150} />
+              </View>
+            </View>
+          </View>
+          <Link href="/hexagons" asChild>
+            <TouchableOpacity style={styles.orderButton}>
+              <Text style={styles.orderButtonText}>SELECT</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </ScrollView>    
+    </SafeAreaView>
   );
 };
 
 export default Products;
 
-type ItemProps = { 
-  title: string, 
-  image: string, 
-  style : object, 
-  backgroundColor: string, 
-  link: RelativePathString 
-};
-
-const Item = ({ title, image, style, backgroundColor, link }: ItemProps) => (
-  <View style={{ ...style, backgroundColor: backgroundColor }}>
-    <Text style={styles.title}>{title}</Text>
-    <Image style={styles.portrait} source={{ uri:image }} />
-    <Link href={link} asChild>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>See Products</Text>
-      </TouchableOpacity>
-    </Link>
-  </View>
-);
-
 const styles = StyleSheet.create({
   container : {
     display: "flex",
     flexDirection: "column",
+    backgroundColor: "skyblue"
   },
-  section: {
-    display: "flex",
-    flexDirection: "column",
+  section : {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#dddddd",
   },
   portrait: {
     width: 200,
@@ -95,25 +170,145 @@ const styles = StyleSheet.create({
     borderColor: "#FFFFFF",
     borderWidth: 5,
   },
-  title : {
-    fontSize: 36,
+  canvasType: {
+    position: "relative",
+    fontSize: 40,
     fontWeight: "bold",
-    color: "#FFFFFF",
-    margin: 40,
+    fontFamily: "BebasNeue-Regular",
+    marginTop: 30,
   },
-  button: {
-    margin: 30,
-    width: 150,
-    height: 50,
+  orderButton : {
+    backgroundColor: "#ffffff",
+    width: 250,
+    height: 40,
+    margin: 20,
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 50,
-    alignSelf: "center",
-    backgroundColor: "#3d7794",
+    borderRadius: 20,
   },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
+  orderButtonText : {
+    color: "#09759a",
+    fontSize: 24,
     fontWeight: "bold",
+    fontFamily: "BebasNeue-Regular",
+  },
+  splitContainer: {
+    flexDirection: "row",
+    width: 305,
+    height: 180,
+    alignContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+    justifyContent: "space-between",
+    shadowColor: "#000",
+    shadowOffset: { 
+      width: 5, 
+      height: 5 
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  splitPreviewContainer_1: {
+    position: "relative",
+    top: 25,
+  },
+  splitPreviewContainer_2: {
+    position: "relative",
+  },
+  splitPreviewWindow: {
+    width: 75,
+    height: 140,
+    overflow: "hidden",
+  },
+  splitImage_1: {
+    width: 300,
+    height: 170,
+    top: -25,
+  },
+  SplitImage_2: {
+    width: 300,
+    height: 170,
+    position: "absolute",
+    left: -75,
+    top: 0,
+  },
+  splitImage_3: {
+    width: 300,
+    height: 170,
+    position: "absolute",
+    left: -150,
+    top: -25,
+  },
+  splitImage_4: {
+    width: 300,
+    height: 170,
+    position: "absolute",
+    left: -225,
+  },
+  comboImagesContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    columnGap: 10,
+    marginTop: 30,
+    height: 260,
+    justifyContent: "center",
+  },
+  comboColumnImagesContainer: {
+    display: "flex",
+    flexDirection: "column",
+    rowGap: 15,
+    justifyContent: "space-around",
+  },
+  comboImageContainer : {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.5,
+    elevation: 5,
+  },
+  comboImage_1: {
+    width: 70, 
+    height: 90, 
+    resizeMode: "stretch",
+    shadowColor: "#000000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.5,
+    elevation: 5,
+  },
+  comboImage_2: {
+    width: 150, 
+    height: 110, 
+    resizeMode: "stretch",
+    shadowColor: "#000000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.5,
+    elevation: 5,
+    
+  },
+  hexagonImagesContainer: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  hexagonImageContainer : {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  hexagonHorizonalContainer: {
+    display: "flex",
+    flexDirection: "row",
+    top: -50,
   },
 });
