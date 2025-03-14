a9b0c7237bac935886ee69eb7f71d04ee9894245
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-14 08:52:15 +0200

    aligned page title to center

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,11 +6,11 @@ import store from "./store";
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <Stack>
+      <Stack screenOptions={{ headerTitleAlign: "center"}}>
         <Stack.Screen name="combos" options={{ title: "Combos" }} />
         <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="order" options={{ title: "Order" }} />
-        <Stack.Screen name="portraits" options={{ title: "Portraits" }} />
+        <Stack.Screen name="portraitSizes" options={{ title: "PortraitSizes" }} />
         <Stack.Screen name="products" options={{ title: "Products" }} />
         <Stack.Screen name="splits" options={{ title: "Splits" }} />
         <Stack.Screen name="uploadPortrait" options={{ title: "Upload Portrait" }} />
