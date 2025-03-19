a3c797b488408eaa3b3905567bbdceb030624028
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-19 09:16:56 +0200

    formatted according to new design

--- app/splits.tsx
+++ app/splits.tsx
@@ -5,16 +5,24 @@ import Joy from "./components/splits/joy";
 import Nice from "./components/splits/nice";
 import Sweet from "./components/splits/sweet";
 import Home from "./components/splits/home";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Splits = () => {
     
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ justifyContent: "center" }}>
-      <Dine />
-      <Joy />
-      <Nice />
-      <Sweet />
-      <Home />
+      <LinearGradient 
+        colors={["#2dcc9f", "#62004d"]} 
+        start={{ x:0, y: 0 }} 
+        end={{ x: 1, y: 1 }} 
+        style={styles.gradientContainer}   
+      >
+        <Dine />
+        <Joy />
+        <Nice />
+        <Sweet />
+        <Home />
+      </LinearGradient>
     </ScrollView>
   );
 };
@@ -24,6 +32,10 @@ export default Splits;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  gradientContainer: {
+    flex: 1,
+    height: "100%",
   },
   image : {
     marginTop: 20,
