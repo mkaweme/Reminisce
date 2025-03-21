91bbab4f3a7fddf116dfebdcab96e9bfbfdf4170
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-21 09:11:15 +0200

    Made background according to new design

--- app/combos.tsx
+++ app/combos.tsx
@@ -6,24 +6,42 @@ import Warm from "./components/combos/warm";
 import Moments from "./components/combos/moments";
 import Fun from "./components/combos/fun";
 import Growth from "./components/combos/growth";
+import { LinearGradient } from "expo-linear-gradient";
+import { Divider } from "@rneui/base";
 
 const Combos: React.FC = () => {
 
   return (
-    <ScrollView style={styles.container} contentContainerStyle={{ justifyContent: "center" }}>
-      <Cherry />
-      <Warm />
-      <Time />
-      <Moments />
-      <Fun />
-      <Growth />
-    </ScrollView>
+    <LinearGradient 
+      colors={["#2dcc9f", "#62004d"]} 
+      start={{ x:0, y: 0 }} 
+      end={{ x: 1, y: 1 }} 
+      style={styles.gradientContainer}   
+    >
+      <ScrollView style={styles.container} contentContainerStyle={{ justifyContent: "center" }}>
+        <Cherry />
+        <Divider width={1}/>
+        <Warm />
+        <Divider width={1}/>
+        <Time />
+        <Divider width={1}/>
+        <Moments />
+        <Divider width={1}/>
+        <Fun />
+        <Divider width={1}/>
+        <Growth />
+      </ScrollView>
+    </LinearGradient>
   );
 };
 
 export default Combos;
 
 const styles = StyleSheet.create({
+  gradientContainer: {
+    flex: 1,
+    height: "100%",
+  },
   container: {
     flex: 1,
   },
