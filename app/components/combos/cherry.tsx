9a8630e31abf1222481791675b6728a4bd60494f
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-21 09:14:29 +0200

    changed layout to new design

--- app/components/combos/cherry.tsx
+++ app/components/combos/cherry.tsx
@@ -2,17 +2,29 @@ import React, { useState } from "react";
 import { View, Image, StyleSheet, TouchableOpacity, Text } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { Link, usePathname } from "expo-router";
+import { Link, useLocalSearchParams, usePathname } from "expo-router";
+import { cartActions } from "app/CartReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "app/store";
+import { LinearGradient } from "expo-linear-gradient";
+import MaskedView from "@react-native-masked-view/masked-view";
 
+const PRICE: number = 380;
+const SIZE: string = "1 X A3 | 2 X A4";
+const NAME : string = "CHERRY";
 const Cherry = () => {
+
+  const { size, price , type, name } = useLocalSearchParams();
+  const itemPrice = Number(price);
 
   //Define state variables
   const [image_1, setImage_1] = useState<string | null>(null);
   const [image_2, setImage_2] = useState<string | null>(null);
   const [image_3, setImage_3] = useState<string | null>(null);
+  const [noImage, setNoImage] = useState<boolean>(false);
 
-  const pathname = usePathname();
-  console.log("Pathname : ", pathname);
+  const pathName = usePathname();
+
   //Define a function for selecting an image from the device
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -23,67 +35,180 @@ const Cherry = () => {
     
     //If an images was selected and the process wasn't cancelled
     if (!result.canceled) {
+      setNoImage(false);
       return result.assets[0].uri;
     } else return null;
   };
 
+  const cartItems = useSelector((state : RootState) => state.cart.items);
+  const dispatch = useDispatch();
+
+  //Define a function that adds an item to the cart
+  const addItemToCart = () => {
+    if(!image_1 || !image_2 || !image_3) {
+      setNoImage(true);
+      return;
+    }
+    const item = {
+      id: size,
+      name: name,
+      price: itemPrice,
+      size: size,
+      imageUrls: [image_1,image_2, image_3],
+      quantity: 1,
+      totalPrice: itemPrice,
+      type: type
+    };
+    dispatch(cartActions.addToCart(item));
+  };
+  
+  //Define a function that aremoves an item from the cart
+  const removeItemFromCart = () => {
+    const item = {
+      id: size,
+      name: size,
+      price: itemPrice,
+      size: size,
+      imageUrls: [image_1,image_2, image_3],
+      quantity: 1,
+      totalPrice: itemPrice,
+      type: type
+    };
+    dispatch(cartActions.removeFromCart(item));
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.comboType}>3 PIECE CHERRY</Text>
-      <View style={styles.imagesContainer}>
-        <View style={styles.imageContainer}>
-          <Image 
-            source={image_1 ? { uri: image_1 } : { uri : "https://picsum.photos/200/300" }} 
-            style={{ width: 100, height: 141 }} 
-          />
-          {
-            pathname.includes("upload") &&
-          <TouchableOpacity 
-            style={styles.cameraIcon} onPress={async () => setImage_1(await pickImage())}
-          >
-            <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
-          </TouchableOpacity>
-          }
+      <View style={styles.section}>
+        <Text style={styles.canvasType}>3 PIECE {NAME}</Text>
+        <View style={styles.imagesContainer}>
+          <View style={styles.imageContainer}>
+            <Image 
+              source={image_1 ? { uri: image_1 } : { uri : "https://picsum.photos/200/300" }} 
+              style={{ width: 121, height: 158 }} 
+            />
+            {
+              pathName.includes("upload") && (
+                <TouchableOpacity 
+                  style={styles.cameraIcon} onPress={async () => setImage_1(await pickImage())}
+                >
+                  <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+                </TouchableOpacity>
+              )
+            }
+          </View>
+          <View style={styles.imageContainer}>
+            <Image 
+              source={image_2 ? { uri: image_2 } : { uri : "https://picsum.photos/200/300" }} 
+              style={{ width: 144, height: 193, }} 
+            />
+            {
+              pathName.includes("upload") && (
+                <TouchableOpacity 
+                  style={styles.cameraIcon} 
+                  onPress={async () => setImage_2(await pickImage())}
+                >
+                  <MaskedView
+                    maskElement={(
+                      <View
+                        style={[
+                          StyleSheet.absoluteFill, 
+                          { borderWidth : 3, borderRadius: 10 }]}
+                      />
+                    )}
+                    style={[StyleSheet.absoluteFill]}
+                  >
+                    <LinearGradient
+                      colors={["#d900aa", "#34ffc6"]}
+                      start={{ x: 0, y: 0 }}
+                      end={{ x: 1, y: 1 }}
+                      style={[StyleSheet.absoluteFill]}
+                    />
+                  </MaskedView>
+                  <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+                </TouchableOpacity>
+              )
+            }
+          </View>
+          <View style={styles.imageContainer}>
+            <Image 
+              source={image_3 ? { uri: image_3 } : { uri : "https://picsum.photos/200/300" }} 
+              style={{ width: 121, height: 158 }} 
+            />
+            {
+              pathName.includes("upload") && (
+                <TouchableOpacity 
+                  style={styles.cameraIcon} 
+                  onPress={async () => setImage_3(await pickImage())}
+                >
+                  <MaskedView
+                    maskElement={(
+                      <View
+                        style={[
+                          StyleSheet.absoluteFill, 
+                          { borderWidth : 3, borderRadius: 10 }]}
+                      />
+                    )}
+                    style={[StyleSheet.absoluteFill]}
+                  >
+                    <LinearGradient
+                      colors={["#d900aa", "#34ffc6"]}
+                      start={{ x: 0, y: 0 }}
+                      end={{ x: 1, y: 1 }}
+                      style={[StyleSheet.absoluteFill]}
+                    />
+                  </MaskedView>
+                  <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+                </TouchableOpacity>
+              )
+            }
+          </View>
         </View>
-        <View style={styles.imageContainer}>
-          <Image 
-            source={image_2 ? { uri: image_2 } : { uri : "https://picsum.photos/200/300" }} 
-            style={{ width: 141, height: 200, }} 
-          />
-          <TouchableOpacity 
-            style={styles.cameraIcon} 
-            onPress={async () => setImage_2(await pickImage())}
+        <View style={styles.details}>
+          <Text style={styles.canvasDimensions}>{SIZE}</Text>
+          <LinearGradient
+            colors={["#34ffc6", "#d900aa" ]} 
+            start={{ x:0, y: 0 }} 
+            end={{ x: 1, y: 1 }} 
+            style={styles.priceContainer}   
           >
-            <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
-          </TouchableOpacity>
+            <Text style={styles.price}>K{PRICE}</Text>
+          </LinearGradient>
         </View>
-        <View style={styles.imageContainer}>
-          <Image 
-            source={image_3 ? { uri: image_3 } : { uri : "https://picsum.photos/200/300" }} 
-            style={{ width: 100, height: 141 }} 
-          />
-          <TouchableOpacity 
-            style={styles.cameraIcon} 
-            onPress={async () => setImage_3(await pickImage())}
-          >
-            <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
-          </TouchableOpacity>
-        </View>
+        { 
+          noImage ? (
+            <Text style={styles.warning}>
+              Please upload all images before adding an item to cart
+            </Text>
+          ) : null
+        }
+        {
+          pathName.includes("upload") ? (
+            cartItems.some((value) => value.size == size ) ? (
+              <TouchableOpacity style={styles.orderButton} onPress={removeItemFromCart}>
+                <Text style={styles.orderButtonText}>REMOVE FROM CART</Text>
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity style={styles.orderButton} onPress={addItemToCart}>
+                <Text style={styles.orderButtonText}>ADD TO CART</Text>
+              </TouchableOpacity>
+            )
+          ) : (
+            <Link href={{
+              pathname: "/uploadCombo",
+              params: {
+                name: NAME,
+              }
+            }}
+            asChild
+            >
+              <TouchableOpacity style={styles.orderButton}>
+                <Text style={styles.orderButtonText}>SELECT</Text>
+              </TouchableOpacity>
+            </Link>
+          )
+        }   
       </View>
-      <View style={styles.details}>
-        <Text style={styles.splitCanvasDimensions}>1 X A3 | 2 X A4</Text>
-        <View style={styles.priceTab}>
-          <Text style={styles.priceTabText}>K750</Text>
-        </View>
-      </View>
-      {
-        pathname.includes("combos") &&
-        <Link href="/uploadCombo" asChild>
-          <TouchableOpacity style={styles.orderButton}>
-            <Text style={styles.orderButtonText}>SELECT</Text>
-          </TouchableOpacity>
-        </Link>
-      }
     </View>
   );
 };
@@ -95,20 +220,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 16,
-    backgroundColor: "#e0b0ff",
   },
-  comboType: {
+  section: {
+    height: 490,
+    width: "96%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 12,
+  },
+  canvasType: {
+    position: "relative",
     fontSize: 40,
-    fontWeight: "bold",
     fontFamily: "BebasNeue-Regular",
-    marginTop: 30,
+    marginTop: 20,
+    color: "#ffffff"
   },
   imagesContainer: {
     display: "flex",
     flexDirection: "row",
     width: "100%",
-    rowGap: 10,
     marginTop: 30,
     alignItems: "center",
     justifyContent: "space-between",
@@ -118,7 +248,6 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "yellow",
     shadowColor: "#000000",
     shadowOffset: { width: 2, height: 2 },
     shadowOpacity: 0.25,
@@ -127,53 +256,51 @@ const styles = StyleSheet.create({
   },
   cameraIcon: {
     position: "absolute",
-    borderColor: "#ffffff66",
-    borderWidth: 3,
-    borderRadius: 25,
     padding: 5,
-    bottom: 0,
-    right: 0,
+    bottom: 5,
+    right: 4,
   },
   details: {
-    width: 275,
-    marginTop: 15,
-    backgroundColor: "#ffffff66",
-    alignContent: "center",
-    alignItems: "center",
-  },
-  splitCanvasDimensions: {
-    fontSize: 30,
-    fontWeight: "bold",
-    fontFamily: "BebasNeue-Regular",
-  },
-  priceTab: {
-    display: "flex",
+    width: 300,
     flexDirection: "row",
-    marginBottom: 10,
-    width: 100,
+    marginTop: 20,
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
-    backgroundColor: "#09759a",
   },
-  priceTabText: {
-    color: "#fff",
+  canvasDimensions: {
     fontSize: 30,
-    fontWeight: "bold",
     fontFamily: "BebasNeue-Regular",
+    color: "#ffffff",
+  },
+  priceContainer: {
+    fontSize: 40,
+    width: 100,
+    height: 50,
+    fontFamily: "BebasNeue-Regular",
+    color: "#ffffff",
+    alignItems: "center",
+    borderRadius:5,
+  },
+  price: {
+    fontFamily: "BebasNeue-Regular",
+    color: "#ffffff",
+    fontSize: 40,
+  },
+  warning : {
+    color: "white",
   },
   orderButton : {
-    width: 150,
+    backgroundColor: "#ffffff",
+    width: 250,
     height: 40,
     margin: 20,
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 20,
-    backgroundColor: "#09759a",
   },
   orderButtonText : {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#ffffff",
+    color: "#1f1f1f",
+    fontSize: 18,
+    fontWeight: "500",
   },
 });
