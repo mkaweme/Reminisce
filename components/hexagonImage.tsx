1847d84d5f92defcc858de37d7e15ade81a3990d
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-25 12:28:40 +0200

    updated imports

---
+++ components/hexagonImage.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Svg, { Defs, ClipPath, Polygon, Image } from "react-native-svg";
+
+interface HexagonImageProps {
+  source: { uri: string };
+  size?: number;
+  shadowOffset?: number;
+  shadowColor?: string;
+}
+
+const HexagonImage: React.FC<HexagonImageProps> = ({ 
+  source, 
+  size = 200,
+}) => {
+  const points = `
+    ${size * 0.5},0 
+    ${size},${size * 0.25}
+    ${size},${size * 0.75}
+    ${size * 0.5},${size}
+    0,${size * 0.75},
+    0,${size * 0.25}
+  `;
+  
+  return (
+    <View style={styles.hexagonContainer}>
+      <Svg width={size} height={size}>
+        <Defs>
+          <ClipPath id="clipHexagon">
+            <Polygon points={points} />
+          </ClipPath>
+        </Defs>
+        <Image 
+          href={source.uri} 
+          width={size} 
+          height={size} 
+          preserveAspectRatio="xMidYMid slice" 
+          clipPath="url(#clipHexagon)" 
+        />
+      </Svg>
+    </View>
+  );
+};
+
+export default HexagonImage;
+
+const styles = StyleSheet.create({
+  hexagonContainer : {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
