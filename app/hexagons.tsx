64f9f850e558608d8bea138ebbdac779e9c52993
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-25 12:22:41 +0200

    formatted according to new design

--- app/hexagons.tsx
+++ app/hexagons.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { Link } from "expo-router";
-import HexagonImage from "./components/hexagonImage";
+import HexagonImage from "../components/hexagonImage";
+import { LinearGradient } from "expo-linear-gradient";
+import MaskedView from "@react-native-masked-view/masked-view";
+import { RootState } from "./store";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "./CartReducer";
 
+const PRICE:number = 680;
+const NAME: string = "HEXAGONS";
+const SIZE: string = "3 X 30cm X 30CM";
 const Hexagons = () => {
+  
   //Define state variables
   const [image_1, setImage_1] = useState<string | null>(null);
   const [image_2, setImage_2] = useState<string | null>(null);
   const [image_3, setImage_3] = useState<string | null>(null);
+  const [noImage, setNoImage] = useState<boolean>(false);
 
+  const cartItems = useSelector((state : RootState) => state.cart.items);
+  const dispatch = useDispatch();
   //Define a function for selecting an image from the device
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -21,83 +32,195 @@ const Hexagons = () => {
     
     //If an images was selected and the process wasn't cancelled
     if (!result.canceled) {
+      setNoImage(false);
       return result.assets[0].uri;
     } else return null;
   };
 
+  //Define a function that adds an item to the cart
+  const addItemToCart = () => {
+    if(!image_1 || !image_2 || !image_3) {
+      setNoImage(true);
+      return;
+    }
+    const item = {
+      id: SIZE,
+      name: NAME,
+      price: PRICE,
+      size: SIZE,
+      imageUrls: [image_1, image_2, image_3],
+      quantity: 1,
+      totalPrice: PRICE,
+      type: SIZE
+    };
+    dispatch(cartActions.addToCart(item));
+  };
+
+  //Define a function that aremoves an item from the cart
+  const removeItemFromCart = () => {
+    const item = {
+      id: SIZE,
+      name: NAME,
+      price: PRICE,
+      size: SIZE,
+      imageUrls: [image_1, image_2, image_3],
+      quantity: 1,
+      totalPrice: PRICE,
+      type: SIZE
+    };
+    dispatch(cartActions.removeFromCart(item));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.canvasType}>Hexagons</Text>
-      <View style={styles.imagesContainer}>
-        <View style={styles.imageContainer}>
-          <HexagonImage 
-            source={image_1 ? { uri: image_1 } : { uri : "https://picsum.photos/200/300" }} 
-            size={150} 
-          />
-          <TouchableOpacity 
-            style={styles.cameraIcon_1} 
-            onPress={async () => setImage_1(await pickImage())}
+    <LinearGradient 
+      colors={["#2dcc9f", "#62004d"]} 
+      start={{ x:0, y: 0 }} 
+      end={{ x: 1, y: 1 }} 
+      style={styles.gradientContainer}   
+    >
+      <ScrollView style={styles.container} contentContainerStyle={{ alignItems: "center" }}>
+        <Text style={styles.canvasType}>Hexagons</Text>
+        <View style={styles.imagesContainer}>
+          <View style={styles.imageContainer}>
+            <HexagonImage 
+              source={image_1 ? { uri: image_1 } : { uri : "https://picsum.photos/200/300" }} 
+              size={150} 
+            />
+            <TouchableOpacity 
+              style={styles.cameraIcon_1} 
+              onPress={async () => setImage_1(await pickImage())}
+            >
+              <MaskedView
+                maskElement={(
+                  <View
+                    style={[
+                      StyleSheet.absoluteFill, 
+                      { borderWidth : 3, borderRadius: 10 }]}
+                  />
+                )}
+                style={[StyleSheet.absoluteFill]}
+              >
+                <LinearGradient
+                  colors={["#d900aa", "#34ffc6"]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                  style={[StyleSheet.absoluteFill]}
+                />
+              </MaskedView>
+              <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.horizonalContainer}>
+            <View style={styles.imageContainer}>
+              <HexagonImage 
+                source={image_2 ? { uri: image_2 } : { uri : "https://picsum.photos/200/300" }} 
+                size={150} 
+              />
+              <TouchableOpacity 
+                style={styles.cameraIcon_2} 
+                onPress={async () => setImage_2(await pickImage())}
+              >
+                <MaskedView
+                  maskElement={(
+                    <View
+                      style={[
+                        StyleSheet.absoluteFill, 
+                        { borderWidth : 3, borderRadius: 10 }]}
+                    />
+                  )}
+                  style={[StyleSheet.absoluteFill]}
+                >
+                  <LinearGradient
+                    colors={["#d900aa", "#34ffc6"]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 1 }}
+                    style={[StyleSheet.absoluteFill]}
+                  />
+                </MaskedView>
+                <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.imageContainer}>
+              <HexagonImage 
+                source={image_3 ? { uri: image_3 } : { uri : "https://picsum.photos/200/300" }} 
+                size={150} 
+              />
+              <TouchableOpacity 
+                style={styles.cameraIcon_2} 
+                onPress={async () => setImage_3(await pickImage())}
+              >
+                <MaskedView
+                  maskElement={(
+                    <View
+                      style={[
+                        StyleSheet.absoluteFill, 
+                        { borderWidth : 3, borderRadius: 10 }]}
+                    />
+                  )}
+                  style={[StyleSheet.absoluteFill]}
+                >
+                  <LinearGradient
+                    colors={["#d900aa", "#34ffc6"]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 1 }}
+                    style={[StyleSheet.absoluteFill]}
+                  />
+                </MaskedView>
+                <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.canvasDimensions}>{SIZE}</Text>
+          <LinearGradient
+            colors={["#34ffc6", "#d900aa" ]} 
+            start={{ x:0, y: 0 }} 
+            end={{ x: 1, y: 1 }} 
+            style={styles.priceContainer}   
           >
-            <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
-          </TouchableOpacity>
+            <Text style={styles.price}>K{PRICE}</Text>
+          </LinearGradient>
         </View>
-        <View style={styles.horizonalContainer}>
-          <View style={styles.imageContainer}>
-            <HexagonImage 
-              source={image_2 ? { uri: image_2 } : { uri : "https://picsum.photos/200/300" }} 
-              size={150} 
-            />
-            <TouchableOpacity 
-              style={styles.cameraIcon_2} 
-              onPress={async () => setImage_2(await pickImage())}
-            >
-              <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+        { 
+          noImage ? (
+            <Text style={styles.warning}>
+              Please upload all images before adding an item to cart
+            </Text>
+          ) : null
+        }
+        {
+          cartItems.some((value) => value.size == SIZE ) ? (
+            <TouchableOpacity style={styles.orderButton} onPress={removeItemFromCart}>
+              <Text style={styles.orderButtonText}>REMOVE FROM CART</Text>
             </TouchableOpacity>
-          </View>
-          <View style={styles.imageContainer}>
-            <HexagonImage 
-              source={image_3 ? { uri: image_3 } : { uri : "https://picsum.photos/200/300" }} 
-              size={150} 
-            />
-            <TouchableOpacity 
-              style={styles.cameraIcon_2} 
-              onPress={async () => setImage_3(await pickImage())}
-            >
-              <MaterialCommunityIcons name="camera-plus-outline" size={24} color="white" />
+          ) : (
+            <TouchableOpacity style={styles.orderButton} onPress={addItemToCart}>
+              <Text style={styles.orderButtonText}>ADD TO CART</Text>
             </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-      <View style={styles.details}>
-        <Text style={styles.canvasDimensions}>3 X A4</Text>
-        <View style={styles.priceTab}>
-          <Text style={styles.priceTabText}>K650</Text>
-        </View>
-      </View>
-      <Link href="/order" asChild>
-        <TouchableOpacity style={styles.orderButton}>
-          <Text style={styles.orderButtonText}>ORDER</Text>
-        </TouchableOpacity>
-      </Link>
-    </View>
+          )
+        } 
+      </ScrollView>
+    </LinearGradient>
   );
 };
 
 export default Hexagons;
 
 const styles = StyleSheet.create({
+  gradientContainer: {
+    flex: 1,
+    height: "100%",
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 16,
-    backgroundColor: "#e0b0ff",
   },
   canvasType: {
+    position: "relative",
     fontSize: 40,
-    fontWeight: "bold",
     fontFamily: "BebasNeue-Regular",
-    margin: 15,
+    marginTop: 20,
+    color: "#ffffff"
   },
   imagesContainer: {
     display: "flex",
@@ -114,67 +237,62 @@ const styles = StyleSheet.create({
   },
   cameraIcon_1: {
     position: "absolute",
-    borderColor: "#ffffff66",
-    borderWidth: 3,
-    borderRadius: 25,
     padding: 5,
     bottom: 100,
-    right: 0,
-  },
-  cameraIcon_2: {
-    position: "absolute",
-    borderColor: "#ffffff66",
-    borderWidth: 3,
-    borderRadius: 25,
-    padding: 5,
-    bottom: 0,
     right: 0,
   },
   horizonalContainer: {
     display: "flex",
     flexDirection: "row",
-    top: -50,
+    top: -38,
+  },
+  cameraIcon_2: {
+    position: "absolute",
+    padding: 5,
+    bottom: 0,
+    right: 0,
   },
   details: {
-    width: 275,
-    marginTop: 15,
-    backgroundColor: "#ffffff66",
-    alignContent: "center",
+    width: 300,
+    flexDirection: "row",
+    marginTop: 20,
+    justifyContent: "space-between",
     alignItems: "center",
   },
   canvasDimensions: {
     fontSize: 30,
-    fontWeight: "bold",
     fontFamily: "BebasNeue-Regular",
+    color: "#ffffff",
   },
-  priceTab: {
-    display: "flex",
-    flexDirection: "row",
-    marginBottom: 10,
+  priceContainer: {
+    fontSize: 40,
     width: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
-    backgroundColor: "#09759a",
-  },
-  priceTabText: {
-    color: "#fff",
-    fontSize: 30,
-    fontWeight: "bold",
+    height: 50,
     fontFamily: "BebasNeue-Regular",
+    color: "#ffffff",
+    alignItems: "center",
+    borderRadius:5,
+  },
+  price: {
+    fontFamily: "BebasNeue-Regular",
+    color: "#ffffff",
+    fontSize: 40,
+  },
+  warning : {
+    color: "white",
   },
   orderButton : {
-    width: 150,
+    width: 250,
     height: 40,
     margin: 20,
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 20,
-    backgroundColor: "#09759a",
+    backgroundColor: "#ffffff",
   },
   orderButtonText : {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#ffffff",
-  }, 
+    color: "#1f1f1f",
+    fontSize: 18,
+    fontWeight: "500",
+  },
 });
