9e8bb6c04ad07ceab7a4dec86b19d6d51df33af7
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-25 12:24:17 +0200

    updated components imports

--- app/splits.tsx
+++ app/splits.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
-import Dine from "./components/splits/dine";
-import Joy from "./components/splits/joy";
-import Nice from "./components/splits/nice";
-import Sweet from "./components/splits/sweet";
-import Home from "./components/splits/home";
+import Dine from "../components/splits/dine";
+import Joy from "../components/splits/joy";
+import Nice from "../components/splits/nice";
+import Sweet from "../components/splits/sweet";
+import Home from "../components/splits/home";
 import { LinearGradient } from "expo-linear-gradient";
 
 const Splits: React.FC  = () => {
     
   return (
     <LinearGradient 
-      colors={["#2dcc9f", "#62004d"]} 
+      colors={["#253f4b", "#446879"]} 
       start={{ x:0, y: 0 }} 
       end={{ x: 1, y: 1 }} 
       style={styles.gradientContainer}   
