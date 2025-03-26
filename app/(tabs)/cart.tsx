9ca468ee23a58e383d2e5bfe3ddb6f516e3d4273
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-26 10:36:16 +0200

    Second commit

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,11 +1,67 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Modal, ScrollView, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import Cherry from "components/combos/cherry";
+import Dine from "components/splits/dine";
+import Fun from "components/combos/fun";
+import Growth from "components/combos/growth";
+import Hexagons from "app/hexagons";
+import Home from "components/splits/home";
+import Joy from "components/splits/joy";
+import Moments from "components/combos/moments";
+import Nice from "components/splits/nice";
+import Sweet from "components/splits/sweet";
+import Time from "components/combos/time";
+import Warm from "components/combos/warm";
+import { useSelector } from "react-redux";
+import { RootState } from "app/store";
 
-const Cart = () => {
+export type OpenMapArgs = {
+  lat: string | number;
+  lng: string | number;
+  label: string;
+};
+
+const Cart: React.FC = () => {
+
+  //Define state variables
+  const [showModal, setShowModal] = useState<boolean>(false);
+  
+  const cart = useSelector((state: RootState) => state.cart);
+  console.log("Cart", cart.items);
+
+  const componentsMap = {
+    CHERRY: Cherry,
+    DINE: Dine,
+    FUN: Fun,
+    GROWTH: Growth,
+    HEXAGONS: Hexagons,
+    HOME: Home,
+    JOY: Joy,
+    MOMENTS: Moments,
+    NICE: Nice,
+    SWEET: Sweet,
+    TIME: Time,
+    WARM: Warm,
+  };
+  
+  // const Component = 
+  //     typeof nameString === "string" && nameString in componentsMap 
+  //       ? componentsMap[name as keyof typeof componentsMap] 
+  //       : null;
+
   return (
-    <View>
+    <ScrollView>
+      <Modal visible={showModal} onRequestClose={() => setShowModal(false)}>
+
+      </Modal>
       <Text>Cart</Text>
-    </View>
+      <ScrollView>
+        {cart.items.map((item, index) => {
+          const Component = componentsMap[item.name as keyof typeof componentsMap];
+          return <Component key={index} />;
+        })}
+      </ScrollView>
+    </ScrollView>
   );
 };
 
