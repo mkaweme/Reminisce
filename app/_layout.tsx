2e9b77365a616c6233bc9907774d45184ffbe411
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-27 16:14:02 +0200

    Removed unused pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,18 +7,9 @@ export default function RootLayout() {
   return (
     <Provider store={store}>
       <Stack screenOptions={{ headerTitleAlign: "center" }}>
-        {/* <Stack.Screen name="combos" options={{ title: "Combos" }} /> */}
+        <Stack.Screen name="combos" options={{ title: "Combos" }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="index" options={{ title: "Home" }} /> */}
         <Stack.Screen name="+not-found"/>
-        {/* <Stack.Screen name="order" options={{ title: "Order" }} />
-        <Stack.Screen name="portraitSizes" options={{ title: "Portrait Sizes" }} />
-        <Stack.Screen name="products" options={{ title: "Products" }} />
-        <Stack.Screen name="splits" options={{ title: "Splits" }} />
-        <Stack.Screen name="test" options={{ title: "Test" }} />
-        <Stack.Screen name="uploadPortrait" options={{ title: "Upload Portrait" }} />
-        <Stack.Screen name="uploadSplit" options={{ title: "Upload Image" }} />
-        <Stack.Screen name="uploadCombo" options={{ title: "Upload Images" }} /> */}
       </Stack>  
     </Provider>
   );
