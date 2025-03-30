7c8e66ff1b16a443bcdf60434fc5aaf97f9e3e7a
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-03-30 22:44:02 +0200

    root layout styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 import store from "./store";
+import BebasNeueRegular from "../assets/fonts/BebasNeue-Regular.ttf";
+import GaladaRegular from "../assets/fonts/Galada-Regular.ttf";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+
+  const [fontsLoaded] = useFonts({
+    "BebasNeue-Regular": BebasNeueRegular,
+    "Galada-Regular":   GaladaRegular,
+  });
+  
+  if (!fontsLoaded) {
+    return null;
+  }
+    
   return (
     <Provider store={store}>
       <Stack screenOptions={{ headerTitleAlign: "center" }}>
-        <Stack.Screen name="combos" options={{ title: "Combos" }} />
+        <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found"/>
       </Stack>  
     </Provider>
   );
-}
+}  
