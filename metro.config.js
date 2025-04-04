3e44478d1741b3ea36955faf5b15c6d0c279982d
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-04-04 08:16:47 +0200

    added react-native-animated config settings

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+// metro.config.js
+const {
+  wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
+
+module.exports = wrapWithReanimatedMetroConfig(config);
