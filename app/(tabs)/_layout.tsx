3dff75b0685f65f5ea48c2a9d9c4fd505e3382d8
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-04-14 17:37:56 +0200

    Added number showing cart items

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import { Tabs } from "expo-router";
 import Feather from "@expo/vector-icons/Feather";
 import Entypo from "@expo/vector-icons/Entypo";
+import { Platform, Text } from "react-native";
+import { useSelector } from "react-redux";
+import { RootState } from "app/store";
 
 const _layout = () => {
+
+  const cart = useSelector((state: RootState) => state.cart);
+    
   return (
-    <Tabs>
+    <Tabs screenOptions={{ tabBarStyle: { 
+      position: "absolute", 
+      height:  Platform.OS === "ios" ? 70 : 55, 
+      backgroundColor: "#ffffff" 
+    } }}
+    >
       <Tabs.Screen 
         name="(products)" 
         options={{
@@ -22,7 +33,14 @@ const _layout = () => {
           headerTitleAlign: "center",
           title: "Cart",
           tabBarIcon: ({ color }) => (
-            <Feather name="shopping-cart" size={24} color={color} /> 
+            <>
+              {cart.items.length > 0 && ( 
+                <Text style={styles.cartNumber}>
+                  {cart.items.length}
+                </Text>
+              )}
+              <Feather name="shopping-cart" size={24} color={color} />
+            </>
           ),
         }} 
       />
@@ -31,3 +49,18 @@ const _layout = () => {
 };
 
 export default _layout;
+
+const styles = {
+  cartNumber: {
+    position: "absolute" as const,
+    top: -4,
+    right: -8,
+    width: 20,
+    backgroundColor: "#b90e0aee",
+    color: "#ffffff",
+    textAlign: "center" as const,
+    borderRadius: 10,
+    zIndex: 1,
+    fontSize: 12,
+  },
+};
